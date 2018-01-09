bc3640f018ea2127b781d6857787ae2f75c11c21
Amir Ezati <ezati@md1.com>
2018-01-09 14:20:23 +0330

    Commit interface

---
+++ Interface/IComparable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BNCSharp_Tests.Interface
+{
+    interface IComparable  //Specific functionality that classes can share
+    {//I allows us to use the members of a class without having to know the exact type of the class
+        int Compare(object o);
+    }
+
+    class Circle : IComparable //Same Notation as classinheritance
+                               // use comma-separated list to define multiple interfaces for inheritance
+
+    {
+        int r;// a field to hold the radious of the circle
+
+        public int Compare(object o)// we must now define the method compare
+        {// the implemented membermust be public with the same sigature
+            return r - (o as Circle).r;
+        }
+    }
+
+    class MyClass
+    {
+        public static object Largest(IComparable a, IComparable b)// this method will work with all classes that implement IComparable regardless of their type
+        {
+            return (a.Compare(b) > 0) ? a : b;
+        }
+    }
+
+    
+}
---
+++ Interface/IMyInterface.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BNCSharp_Tests.Interface
+{
+    //the second way we use interfaces is ...provide an actual interface for the class outwards
+    interface IMyInterface //default internal AM// Naming convention
+    {
+        //Code Block only contains signature
+        //just methods, properties, indexers, eveents
+        //No body or implementation
+        //No Access Modifiers for members
+        //Default and always public AM for members
+
+        void Relevat();
+    }
+
+    // this abstraction have 2 benefits:    access to only relevant members of that class
+    // a more flexible class . implement a class any where we want
+
+
+    class MyClass2 : IMyInterface
+    {
+        public void Relevat()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
